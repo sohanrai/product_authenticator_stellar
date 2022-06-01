30efeb1d8c634f2cf7bc316599e7abd7a4fa4483
SohanRai <sohanmrai@gmail.com>
2022-06-01 12:55:26 +0530

    removed errors

--- lib/Screens/add_product.dart
+++ lib/Screens/add_product.dart
@@ -8,8 +8,59 @@ class add_product extends StatefulWidget {
 }
 
 class _add_productState extends State<add_product> {
+  TextEditingController titleController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: <Widget>[
+          const SizedBox(
+            height: 40.0,
+            width: 150.0,
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Enter Product Details',
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 25),
+            ),
+          ),
+          const SizedBox(
+            height: 100.0,
+            width: 150.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              controller: titleController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Product Title',
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+            width: 150.0,
+            child: Divider(
+              thickness: 0.8,
+              color: Colors.white70,
+            ),
+          ),
+          Container(
+            height: 50,
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: ElevatedButton(
+                child: const Text('Generate QR Code'), onPressed: () {}),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/models/Strings.dart
+++
@@ -1,4 +0,0 @@
-class Strings {
-  static const String qrScanDescription =
-      "Scan the restaurant’s QR Code to View the Menu or click on “Browse Menu” and start searching for Restaurant name.";
-}
